f8d5dbe4dfe1c5d222202759013f00ff8b7c0baf
SanzidaTAnvir <robintanvir289@gmail.com>
2026-05-18 01:15:30 +0600

    Add files via upload

--- admin.js
+++ admin.js
@@ -2,7 +2,10 @@ import { db } from './firebase.js';
 
 import {
 collection,
-addDoc
+addDoc,
+getDocs,
+deleteDoc,
+doc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 /* ADD PRODUCT */
@@ -17,20 +20,16 @@ const img = document.getElementById("img").value;
 
 if(name === "" || price === "" || img === ""){
 
-alert("Please Fill All Fields");
+alert("Fill All Fields");
 
 return;
 
 }
 
-try{
-
 await addDoc(collection(db,"products"),{
 
 name:name,
-
 price:Number(price),
-
 img:img
 
 });
@@ -39,17 +38,62 @@ document.getElementById("msg").innerHTML =
 "✅ Product Added Successfully";
 
 document.getElementById("name").value = "";
-
 document.getElementById("price").value = "";
-
 document.getElementById("img").value = "";
 
-}catch(error){
-
-console.log(error);
-
-alert("Firebase Error");
+loadProducts();
 
 }
+
+/* LOAD PRODUCTS */
+
+const productBox = document.getElementById("product-list");
+
+async function loadProducts(){
+
+productBox.innerHTML = "";
+
+const querySnapshot =
+await getDocs(collection(db,"products"));
+
+querySnapshot.forEach((item)=>{
+
+const p = item.data();
+
+productBox.innerHTML += `
+
+<div class="product-card">
+
+<img src="${p.img}">
+
+<div class="product-info">
+
+<h3>${p.name}</h3>
+
+<p>৳ ${p.price}</p>
+
+<button onclick="deleteProduct('${item.id}')">
+Delete
+</button>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+}
+
+loadProducts();
+
+/* DELETE PRODUCT */
+
+window.deleteProduct = async function(id){
+
+await deleteDoc(doc(db,"products",id));
+
+loadProducts();
 
 }
