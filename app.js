f0e7a8dc6318dc1c9a48133875beda097fea74dd
SanzidaTAnvir <robintanvir289@gmail.com>
2026-05-17 16:53:01 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,39 +1,105 @@
-import { db } from "./firebase.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { db } from './firebase.js';
 
-/* CART */
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
+import {
+collection,
+getDocs
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-/* LOAD PRODUCTS */
+/* PRODUCTS LOAD */
+
+const box = document.getElementById("products");
+
 async function loadProducts(){
 
-let box = document.getElementById("products");
-box.innerHTML = "";
+const querySnapshot = await getDocs(collection(db,"products"));
 
-let snap = await getDocs(collection(db,"products"));
+querySnapshot.forEach((doc)=>{
 
-snap.forEach((d)=>{
-
-let p = d.data();
+const p = doc.data();
 
 box.innerHTML += `
+
 <div class="card">
+
 <img src="${p.img}">
+
+<div class="card-content">
+
 <h3>${p.name}</h3>
-<p>৳${p.price}</p>
-<button onclick='addCart("${p.name}",${p.price})'>Add to Cart</button>
+
+<p>৳ ${p.price}</p>
+
+<button onclick='addCart("${p.name}",${p.price})'>
+Add To Cart
+</button>
+
 </div>
+
+</div>
+
 `;
+
 });
 
 }
 
-window.addCart = function(name,price){
+loadProducts();
 
-cart.push({name,price});
-localStorage.setItem("cart",JSON.stringify(cart));
-alert("Added to cart");
+/* HERO SLIDER */
+
+let heroSlides = document.querySelectorAll(".hero-slide");
+
+let currentHero = 0;
+
+setInterval(()=>{
+
+heroSlides[currentHero].classList.remove("active");
+
+currentHero++;
+
+if(currentHero >= heroSlides.length){
+currentHero = 0;
+}
+
+heroSlides[currentHero].classList.add("active");
+
+},4000);
+
+/* MOBILE MENU */
+
+let menuBtn = document.querySelector(".menu-btn");
+
+let nav = document.querySelector("nav");
+
+menuBtn.onclick = ()=>{
+nav.classList.toggle("active");
+}
+
+/* CART */
+
+function updateCartCount(){
+
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+document.getElementById("cart-count").innerText = cart.length;
 
 }
 
-loadProducts();
+updateCartCount();
+
+window.addCart = function(name,price){
+
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+cart.push({
+name,
+price
+});
+
+localStorage.setItem("cart",JSON.stringify(cart));
+
+updateCartCount();
+
+alert("Added To Cart");
+
+}
--- firebase.js
+++ firebase.js
@@ -1,20 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { 
-getFirestore,
-collection,
-addDoc,
-getDocs,
-deleteDoc,
-doc,
-updateDoc
+
+import {
+  getFirestore
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-apiKey: "YOUR_API_KEY",
-authDomain: "YOUR_AUTH_DOMAIN",
-projectId: "YOUR_PROJECT_ID",
-storageBucket: "YOUR_BUCKET"
+  apiKey: "AIzaSyBKjXOcCkcxTtygfGE2OHtYeDOU4OItXPc",
+  authDomain: "tanvir-shop-b170b.firebaseapp.com",
+  projectId: "tanvir-shop-b170b",
+  storageBucket: "tanvir-shop-b170b.firebasestorage.app",
+  messagingSenderId: "724154708231",
+  appId: "1:724154708231:web:9bc3a56a5aabce3b7555a2",
+  measurementId: "G-6WXBVZPYWL"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
