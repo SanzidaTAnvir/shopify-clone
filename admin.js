40e84365bc2c8954b7a1f4d50ce3035cf2764e0b
SanzidaTAnvir <robintanvir289@gmail.com>
2026-05-17 23:28:32 +0600

    Add files via upload

--- admin.js
+++ admin.js
@@ -5,43 +5,50 @@ collection,
 addDoc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-window.addProduct = async function(){
-
-let name = document.getElementById("name").value;
-
-let price = document.getElementById("price").value;
-import { db } from './firebase.js';
-
-import {
-collection,
-addDoc
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+/* ADD PRODUCT */
 
 window.addProduct = async function(){
 
-let name = document.getElementById("name").value;
+const name = document.getElementById("name").value;
 
-let price = document.getElementById("price").value;
+const price = document.getElementById("price").value;
+
+const img = document.getElementById("img").value;
+
+if(name === "" || price === "" || img === ""){
+
+alert("Please Fill All Fields");
+
+return;
+
+}
+
 try{
 
 await addDoc(collection(db,"products"),{
 
 name:name,
+
 price:Number(price),
+
 img:img
 
 });
-document.getElementById("msg").innerText = "✅ Product Added Successfully";
 
+document.getElementById("msg").innerHTML =
+"✅ Product Added Successfully";
 
 document.getElementById("name").value = "";
+
 document.getElementById("price").value = "";
+
 document.getElementById("img").value = "";
 
 }catch(error){
 
 console.log(error);
-alert("Error Adding Product");
+
+alert("Firebase Error");
 
 }
 
